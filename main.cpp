dc60ac30655b37d2b973bc5d4a52febf097ce015
furkanmani <furkaanmani@gmail.com>
2024-10-31 14:58:56 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float pesha,gjatesia,gjatesia_m;
+  double BMI;
+  string emri,mbiemri;
+  cout<<"Jepni emrin:";cin>>emri;
+  cout<<"Jepni mbiemrin:";cin>>mbiemri;
+
+  cout<<"Jepni peshen:";cin>>pesha;
+  cout<<"Jepni gjatesine:";cin>>gjatesia;
+  gjatesia_m=gjatesia/100;
+  BMI=pesha/(gjatesia_m*gjatesia_m);
+
+  cout<<"Mirsesevini "<<mbiemri<< " "<<emri<< " Pesha juaj eshte :"<<pesha<<"kg"<< " Gjatesia juaj eshte:"<<gjatesia<< "cm ," << "dhe BMI indexi juaj eshte:"<<BMI;
+
+return 0;
+
+  
+}
